1194ed0b2cafae9c9d3ae4942deeb79eb3912a2f
eagleychen(陈鹰) <tonychanchen@gmail.com>
2021-01-28 14:46:06 +0800

    更新p2p header

--- TIoTThridSDK/XP2P-iOS/Classes/AppWrapper.h
+++ TIoTThridSDK/XP2P-iOS/Classes/AppWrapper.h
@@ -8,8 +8,22 @@
 #include <stdint.h>
 #include <string>
 
+//type=1:日志； type=2:json;
+typedef void (*CallBackFunc)(int type, char *format);
+typedef void (*CallBackFuncData)(uint8_t *data, size_t len);
+
+#ifdef BUILD_WITH_ANDROID_JNI
+#include <jni.h>
+
 __attribute__ ((visibility ("default")))
-int startServiceWithXp2pInfo(std::string peername);
+int setJavaCallback(JNIEnv *env, jobject obj, const char* name);
+#endif
+
+__attribute__ ((visibility ("default")))
+int getCommandRequestWithSync(const char *params, char **buf, size_t *len, uint64_t timeout_us);
+
+__attribute__ ((visibility ("default")))
+int startServiceWithXp2pInfo(const char* xp2p_info);
 
 __attribute__ ((visibility ("default")))
 std::string delegateHttpFlv();
@@ -35,6 +49,11 @@ int dataSend(uint8_t *data, size_t len);
 __attribute__ ((visibility ("default")))
 void stopService();
 
+__attribute__ ((visibility ("default")))
+void setNativeCallback(CallBackFunc pcallbackFunc);
+
+__attribute__ ((visibility ("default")))
+void setNativeCallback(CallBackFuncData pcallbackFuncData);
 //#ifdef __cplusplus
 //}
 //#endif

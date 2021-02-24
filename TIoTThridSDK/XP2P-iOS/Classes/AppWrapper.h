6ad579ed8943f3eb67e9295176780ecb67dedd1c
runner <runner@Mac-1614134034041.local>
2021-02-24 02:48:04 +0000

    tencentyun/iot-p2p-build@e413833

--- TIoTThridSDK/XP2P-iOS/Classes/AppWrapper.h
+++ TIoTThridSDK/XP2P-iOS/Classes/AppWrapper.h
@@ -9,7 +9,7 @@
 typedef void (*CallBackFunc)(int type, char *format);
 typedef void (*CallBackFuncData)(uint8_t *data, size_t len);
 
-static const char * VIDEOSDKVERSION = "d219833d";
+static const char * VIDEOSDKVERSION = "64270831";
 
 /*
  * type=0:close通知； type=1:日志； type=2:json;

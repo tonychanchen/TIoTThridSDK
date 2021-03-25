3814a953ed22778e3347c03a53c60185244677eb
runner <runner@Mac-1616656248479.local>
2021-03-25 07:26:09 +0000

    tencentyun/iot-p2p-build@7a72096

--- TIoTThridSDK/XP2P-iOS/Classes/AppWrapper.h
+++ TIoTThridSDK/XP2P-iOS/Classes/AppWrapper.h
@@ -10,7 +10,7 @@ extern "C" {
 
 #define MAX_SIZE_OF_PARAMS 3000
 
-static const char * VIDEOSDKVERSION = "a74f2f4b";
+static const char * VIDEOSDKVERSION = "27cdbce9";
 
 enum XP2PType {
     XP2PTypeClose   = 1000, //数据传输完成
@@ -20,56 +20,150 @@ enum XP2PType {
     XP2PTypeSaveFileOn  = 8000, //获取保存音视频流开关状态
     XP2PTypeSaveFileUrl = 8001 //获取音视频流保存路径
 };
-/*
- * type=0:close通知； type=1:日志； type=2:json;
- * native返回给SDK结果：char*
- */
-typedef char* (*msg_handle_t)(const char *id, XP2PType type, const char* msg);
 
+typedef char* (*msg_handle_t)(const char *id, XP2PType type, const char* msg);
 typedef void (*av_recv_handle_t)(const char *id, uint8_t* recv_buf, size_t recv_len);
 
-void setUserCallbackToXp2p(const char *id, av_recv_handle_t recv_handle, msg_handle_t msg_handle);
+/**
+ * @brief 设置回调函数
+ *
+ * @param recv_handle: 音视频数据回调
+ * @param msg_handle: 控制类消息回调
+ * @return 无返回值
+ */
+void setUserCallbackToXp2p(av_recv_handle_t recv_handle, msg_handle_t msg_handle);
 
+/**
+ * @brief 发送信令消息给camera设备并等待回复，同步阻塞方式
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param command: 可以为任意格式字符或二进制数据，长度由cmd_len提供，建议在16KB以内，否则会影响实时性
+ * @param cmd_len: command长度
+ * @param recv_buf: 用于存放camera回复的数据，内存由接口内部申请外部释放，实际数据长度根据recv_len获取
+ * @param recv_len: camera回复的数据长度
+ * @param timeout_us: 命令超时时间，单位为微秒，值为0时采用默认超时(7500ms左右)
+ * @return 0 为成功
+ */
+int postCommandRequestSync(const char *id, const unsigned char *command, size_t cmd_len,
+        unsigned char **recv_buf, size_t *recv_len, uint64_t timeout_us);
 
-__attribute__ ((visibility ("default")))
-int getCommandRequestWithSync(const char *id, const char *params, char **buf, size_t *len, uint64_t timeout_us);
+/**
+ * @brief 发送信令消息给camera设备，camera回复的数据由注册的回调函数返回，异步非阻塞方式
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param command: 可以为任意格式字符或二进制数据，长度由cmd_len提供，建议在16KB以内，否则会影响实时性
+ * @param cmd_len: command长度
+ * @return 0 为成功
+ */
+int postCommandRequestWithAsync(const char *id, const unsigned char *command, size_t cmd_len);
 
-__attribute__ ((visibility ("default")))
-int getCommandRequestWithAsync(const char *id, const char *params);
+/**
+ * @brief 向camera设备请求媒体流，异步回调方式
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param params: 直播(`action=live`)或回放(`action=playback`)参数
+ * @param crypto: 是否开启传输层加密，如果关闭(crypto=false)，则建议用户在应用层加密，否则有安全风险
+ * @return 请求句柄
+ */
+void *startAvRecvService(const char *id, const char *params, bool crypto);
 
-__attribute__ ((visibility ("default")))
-void *startAvRecvService(const char *id, const char *params);
-
-__attribute__ ((visibility ("default")))
+/**
+ * @brief 关闭媒体流传输
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param req: 接口`startAvRecvService`的返回值，当前版本可传入null
+ * @return 0 为成功
+ */
 int stopAvRecvService(const char *id, void *req);
 
-__attribute__ ((visibility ("default")))
+/**
+ * @brief 初始化xp2p服务
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param product_id: 产品ID
+ * @param device_name: 设备名称
+ * @param xp2p_info_attr: xp2p属性，当前版本固定为`_sys_xp2p_info`，若xp2p_info已获取，该参数可传入null
+ * @param xp2p_info: xp2p信息
+ * @return 0 为成功
+ */
 int startServiceWithXp2pInfo(const char* id, const char *product_id, const char *device_name,
         const char *xp2p_info_attr,  const char* xp2p_info);
 
-__attribute__ ((visibility ("default")))
+/**
+ * @brief 获取本地代理url
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @return 本地代理url
+ */
 const char *delegateHttpFlv(const char *id);
 
-__attribute__ ((visibility ("default")))
-void *runSendService(const char *id);
+/**
+ * @brief 启动向camera设备发送语音或自定义数据服务，异步非阻塞方式
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param params: 请求参数采用 key1=value&key2=value2 格式，key不允许以下划线_开头，且key和value中间不能包含&/+=特殊字符
+ * @param crypto: 是否开启传输层加密，如果关闭(crypto=false)，则建议用户在应用层加密，否则有安全风险
+ * @return 请求句柄
+ */
+void *runSendService(const char *id, const char *params, bool crypto);
 
-__attribute__ ((visibility ("default")))
+/**
+ * @brief 关闭语音发送传输
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param req: 接口`runSendService`的返回值，当前版本可传入null
+ * @return 请求句柄
+ */
 int stopSendService(const char *id, void *req);
 
-__attribute__ ((visibility ("default")))
+/**
+ * @brief 设置云api信息，若不用sdk获取xp2p信息，该接口可不用设置
+ *
+ * @param sec_id: 云API secrct_id
+ * @param sec_key: 云API secrct_key
+ * @return 0 为成功
+ */
 int setQcloudApiCred(const char *sec_id, const char *sec_key);
 
-//__attribute__ ((visibility ("default")))
-//int setDeviceInfo(const char *id, const char *pro_id, const char *dev_name);
-//
-//__attribute__ ((visibility ("default")))
-//int setXp2pInfoAttributes(const char *attr);
-
-__attribute__ ((visibility ("default")))
+/**
+ * @brief 向camera设备发送语音或自定义数据
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param data: 要发送的数据内容
+ * @param len: 要发送的数据长度
+ * @return 0 为成功
+ */
 int dataSend(const char *id, uint8_t *data, size_t len);
 
-__attribute__ ((visibility ("default")))
+/**
+ * @brief 停止xp2p服务
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @return 0 为成功
+ */
 void stopService(const char *id);
+
+/*================================废弃接口=======================================*/
+/**
+ * @brief 发送信令消息给camera设备并等待回复，同步阻塞方式
+ *
+ * @param id: 目标camera在app端的唯一标识符
+ * @param command: 字符格式命令
+ * @param buf: 用于存放camera回复的数据，内存由接口内部申请外部释放，实际数据长度根据len获取
+ * @param len: camera回复的数据长度
+ * @param timeout_us: 命令超时时间，单位为微秒，值为0时采用默认超时(7500ms左右)
+ * @return 0 为成功
+ */
+int getCommandRequestWithSync(const char *id, const char *command, char **buf, size_t *len, uint64_t timeout_us);
+
+/**
+ * @brief 发送信令消息给camera设备，camera回复的数据由注册的回调函数返回，异步非阻塞方式
+ *
+ * @param id:目标camera在app端的唯一标识符
+ * @param command:字符格式命令
+ * @return 0 为成功
+ */
+int getCommandRequestWithAsync(const char *id, const char *command);
 
 #ifdef __cplusplus
 }

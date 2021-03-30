9c6cc3b3a47d9093aca0937085128315b04d4bee
runner <runner@Mac-1617098005256.local>
2021-03-30 10:12:39 +0000

    tencentyun/iot-p2p-build@86db3fd

--- TIoTThridSDK/XP2P-iOS/Classes/AppWrapper.h
+++ TIoTThridSDK/XP2P-iOS/Classes/AppWrapper.h
@@ -10,7 +10,7 @@ extern "C" {
 
 #define MAX_SIZE_OF_PARAMS 3000
 
-static const char * VIDEOSDKVERSION = "931b5809";
+static const char * VIDEOSDKVERSION = "2b1cc8ff";
 
 enum XP2PType {
     XP2PTypeClose   = 1000, //数据传输完成
